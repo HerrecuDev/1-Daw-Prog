472b1761bb651ad92466537c6bc09ac16deb6a15
HerrecuDev <117910026+HerrecuDev@users.noreply.github.com>
2023-01-12 10:32:53 +0100

    Add files via upload

---
+++ Vectores Programación/Arrays/EjercicioComprobacionUtiles.java
@@ -0,0 +1,75 @@
+package ArraysVectores;
+
+
+public class EjercicioComprobacionUtiles {
+    
+    public static void main(String[] args) {
+        
+        int[] array = null;
+
+        array = UtilesArrayPablo.rellenar(10);
+
+        UtilesArrayPablo.imprimir(array);
+
+        array =UtilesArrayPablo.limpiar();
+
+        array =UtilesArrayPablo.insertarAlPrincipio(array, 10);
+
+        array =UtilesArrayPablo.insertarAlPrincipio(array, 20);
+
+        array =UtilesArrayPablo.insertarAlPrincipio(array, 30);
+
+        array =UtilesArrayPablo.insertarAlPrincipio(array, 40);
+
+        array =UtilesArrayPablo.insertarAlPrincipio(array, 50);
+
+        array =UtilesArrayPablo.insertarAlPrincipio(array, 7);
+
+        array =UtilesArrayPablo.insertarAlPrincipio(array, 9);
+
+        array =UtilesArrayPablo.insertarAlPrincipio(array, 98);
+
+        array =UtilesArrayPablo.insertarAlFinal(array, 5);
+
+        boolean ordenado = UtilesArrayPablo.estaOrdenado(array);
+
+        System.out.println("Esta ordenado: " + ordenado);
+
+        UtilesArrayPablo.imprimir(array);
+
+        array =UtilesArrayPablo.eliminarPrimero(array);
+
+        UtilesArrayPablo.imprimir(array);
+
+        array =UtilesArrayPablo.eliminar(array, 30);
+
+        UtilesArrayPablo.imprimir(array);
+
+        array =UtilesArrayPablo.ordenar(array);
+
+        UtilesArrayPablo.imprimir(array);
+
+        array =UtilesArrayPablo.insertarOrdenado(array, 15);
+
+        UtilesArrayPablo.imprimir(array);
+        
+        int posicion15 =UtilesArrayPablo.buscar(array, 15);
+
+        System.out.println("El elemento 15 está en la posicion: " + posicion15);
+
+        int posicion99 =UtilesArrayPablo.buscar(array, 99);
+
+        System.out.println("El elemento 99 está en la posicion: " + posicion99);
+
+        array = UtilesArrayPablo.invertir(array);
+
+        System.out.print("Invierto el array:  ");
+        UtilesArrayPablo.imprimir(array);
+
+        array =UtilesArrayPablo.partirPor(array, 2,5);
+
+        System.out.println("Parto el array por 2 y 5");
+        UtilesArrayPablo.imprimir(array);
+
+    }
+}
---
+++ Vectores Programación/Arrays/UtilesArrayPablo.java
@@ -0,0 +1,27 @@
+public class UtilesArrayPablo 
+{
+    public static void main(String[] args) 
+    {
+        limpiar()
+        rellenar
+
+    }
+
+    /**
+     * 
+     * @return
+     */
+    static int [] limpiar() 
+    {
+        int [] nuevo = new int [0];
+        return nuevo;
+    }
+
+    static rellenar(int numeroElementos) 
+    {
+
+    }
+    
+    
+    
+}
