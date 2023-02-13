5ee8217ad63a733662aeb5682698e0f0dc15a889
Pablo <pablojhc96@hotmail.com>
2023-02-13 10:24:08 +0100

    avance en clase

--- code/T6practicas/Clases/DiscoEj3Refuerzo.java
+++ code/T6practicas/Clases/DiscoEj3Refuerzo.java
@@ -10,7 +10,7 @@ public class DiscoEj3Refuerzo
     private int duracion;
 
 
-    //Contructores:
+    //Contructores: //Coge todos los parametros y atributos de nuestro disco.
     public DiscoEj3Refuerzo(String codigo , String autor , String titulo , String genero, int duracion)
     {
         this.codigo = codigo;
@@ -20,7 +20,82 @@ public class DiscoEj3Refuerzo
         this.duracion = duracion;
     }
 
-    //Metodos :
+
+
+     //Metodos :
+
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+
+    public String getAutor() {
+        return autor;
+    }
+
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+
+    public String getGenero() {
+        return genero;
+    }
+
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+
+
+    @Override
+    public String toString()
+    {
+        String resultado = "";
+        
+
+        resultado += "======================";
+        resultado += ("Autor" + this.autor);
+        resultado += ("Código" + this.codigo);
+        resultado += ("Título" + this.titulo);
+        resultado += ("Género" + this.genero);
+        resultado += "Duración" + this.duracion;
+
+        return resultado;
+
+
+    }
+    
+
+    
 
 
 }
--- code/T6practicas/UsoClases/ColeccionDeDiscosPrincipal.java
+++ code/T6practicas/UsoClases/ColeccionDeDiscosPrincipal.java
@@ -1,6 +1,8 @@
 package code.T6practicas.UsoClases;
 import java.util.Scanner;
 
+import javax.lang.model.util.ElementScanner6;
+
 import code.T6practicas.Clases.DiscoEj3Refuerzo;
 
 /**
@@ -53,10 +55,42 @@ public class ColeccionDeDiscosPrincipal {
         System.out.println("\nLISTADO");
         System.out.println("=======");
 
-        for(DiscoEj3Refuerzo d : discos) {
-          if (!d.getCodigo().equals("LIBRE")) {
-            System.out.println(d);
+        Menu();
+        int listado = s.nextInt();
+        
+        
+        switch (listado) 
+        {
+          case 1:
+          for(DiscoEj3Refuerzo d : discos) {
+            if (!d.getCodigo().equals("LIBRE")) 
+            {
+              System.out.println(d);
+            }
           }
+            break;
+
+          case 2:
+            
+            break;
+          case 3:
+            
+            break;
+          case 4:
+            
+            break;
+        
+          default:
+          System.out.println("Listado INEXISTENTE PRUEBE DE NUEVO");
+          listado = s.nextInt();
+
+            break;
+        }
+
+        for(DiscoEj3Refuerzo d : discos) {
+          // if (!d.getCodigo().equals("LIBRE")) {
+            System.out.println(d);
+          //}
         }
 
         break;
@@ -64,26 +98,38 @@ public class ColeccionDeDiscosPrincipal {
       case 2:
         System.out.println("\nNUEVO DISCO");
         System.out.println("===========");
-        
-        // Busca la primera posición libre del array
-        int primeraLibre = -1;
-        do {
-          primeraLibre++;
-        } while (!((discos[primeraLibre].getCodigo()).equals("LIBRE")));
-        
+
         System.out.println("Por favor, introduzca los datos del disco.");  
         System.out.print("Código: ");
         codigoIntroducido = s.nextLine();
-        System.out.print("Autor: ");
-        autorIntroducido = s.nextLine();
-        System.out.print("Título: ");
-        tituloIntroducido = s.nextLine();
-        System.out.print("Género: ");
-        generoIntroducido = s.nextLine();
-        System.out.print("Duración: ");
-        duracionIntroducida = Integer.parseInt(s.nextLine());
-        discos[primeraLibre] = new DiscoEj3Refuerzo(
-          codigoIntroducido, autorIntroducido, tituloIntroducido, generoIntroducido, duracionIntroducida);
+        
+        // Codigo Unico
+        //En el caso de que no xista dicho codigo lo introduce :
+
+        if (buscarCodigo(discos, codigoIntroducido) == -1) 
+        {
+
+            System.out.print("Autor: ");
+            autorIntroducido = s.nextLine();
+
+            System.out.print("Título: ");
+            tituloIntroducido = s.nextLine();
+
+            System.out.print("Género: ");
+            generoIntroducido = s.nextLine();
+
+            System.out.print("Duración: ");
+            duracionIntroducida = Integer.parseInt(s.nextLine());
+            DiscoEj3Refuerzo discoNuevo = new DiscoEj3Refuerzo(
+            codigoIntroducido, autorIntroducido, tituloIntroducido, generoIntroducido, duracionIntroducida);
+
+              discos = insertarDiscos(discos, discoNuevo);
+        }
+        
+          else
+          {
+            System.out.println("Error Codigo ya introducido");
+          }
         
         break;
         
@@ -94,11 +140,11 @@ public class ColeccionDeDiscosPrincipal {
         System.out.print("Por favor, introduzca el código del disco cuyos datos desea cambiar: ");
         codigoIntroducido = s.nextLine();
   
-        int i = -1;
-        do {
-          i++;
-        } while (!((discos[i].getCodigo()).equals(codigoIntroducido)));
         
+
+        //Buscar Código :
+        int i = buscarCodigo(discos , codigoIntroducido);
+
         System.out.println("Introduzca los nuevos datos del disco o INTRO para dejarlos igual.");
   
         System.out.println("Código: " + discos[i].getCodigo());
@@ -144,13 +190,23 @@ public class ColeccionDeDiscosPrincipal {
         
         System.out.print("Por favor, introduzca el código del disco que desea borrar: ");
         codigoIntroducido = s.nextLine();
+        i = buscarCodigo(discos, codigoIntroducido);
+
+        if ( i != -1) 
+        {
+          discos = borrarDisco (discos , i);
+          System.out.println("Album Borrado");
+        }
+        else 
+        {
+          System.out.println("No existe el disco con el codigo " + codigoIntroducido);
+        }
   
-        i = -1;
-        do {
-          i++;
-        } while (!((discos[i].getCodigo()).equals(codigoIntroducido)));
-        discos[i].setCodigo("LIBRE");
-        System.out.println("Album borrado.");
+        // i = -1;
+        // do {
+        //   i++;
+        // } while (!((discos[i].getCodigo()).equals(codigoIntroducido)));
+        
         
         break;
         
@@ -159,6 +215,76 @@ public class ColeccionDeDiscosPrincipal {
       } // switch
     } while (opcion != 5);
   }
+
+
+  public static DiscoEj3Refuerzo[] insertarDiscos(DiscoEj3Refuerzo [] discos, DiscoEj3Refuerzo disco)
+    {
+        DiscoEj3Refuerzo [] NuevaColeccion = new DiscoEj3Refuerzo [discos.length +1];
+
+        for (int i = 0; i < discos.length; i++) 
+        {
+          NuevaColeccion[i] = discos[i];
+        }
+
+        NuevaColeccion[NuevaColeccion.length-1] = disco;
+        return NuevaColeccion;
+    }
+
+
+    
+    /**
+     * 
+     * @param discos
+     * @param codigoIntroducido
+     * @return
+     */
+    public static int buscarCodigo (DiscoEj3Refuerzo[] discos , String codigoIntroducido)
+    {
+      int posicion = -1;
+
+      for (int i = 0; i < discos.length; i++) 
+      {
+        DiscoEj3Refuerzo disco = discos [i];
+        if (disco.getCodigo().equalsIgnoreCase(codigoIntroducido)) 
+        {
+          posicion = i;
+        }
+
+      }
+      return posicion;
+    }
+
+    public static DiscoEj3Refuerzo [] borrarDisco(DiscoEj3Refuerzo[] discos , int posicion)
+    {
+      DiscoEj3Refuerzo [] NuevaColeccionDiscos = new DiscoEj3Refuerzo[discos.length - 1];
+      for (int i = 0; i < NuevaColeccionDiscos.length; i++) 
+      {
+        if ( i< posicion)
+        {
+          NuevaColeccionDiscos[i] = discos[i];
+        }
+        else if(i > posicion)
+        {
+          NuevaColeccionDiscos[i] = discos [i+1];
+        }
+      }
+      return NuevaColeccionDiscos;
+    }
+
+
+
+    public static void Menu()
+    {
+      
+        
+      System.out.println("1 - VER LISTADO COMPLETO");
+      System.out.println("2 - VER LISTADO POr Autor");
+      System.out.println("1 - VER LISTADO POR GENERO");
+      System.out.println("1 - VER LISTADO X DURACION (RANGO)");
+
+      System.out.println("¿Como quiere ver su listado?");
+
+    }
 
 
 
