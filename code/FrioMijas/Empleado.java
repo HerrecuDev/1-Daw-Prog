544874444140bcca0877358061a65c77c77c3e32
Pablo <pablojhc96@hotmail.com>
2023-03-10 14:34:57 +0100

    ok

---
+++ code/FrioMijas/Empleado.java
@@ -0,0 +1,11 @@
+package code.FrioMijas;
+
+public class Empleados 
+{
+    //Atributos:
+
+    private String nombre;
+    private String apellido;
+    private
+    
+}
---
+++ code/FrioMijas/Persona.java
@@ -0,0 +1,21 @@
+package code.FrioMijas;
+
+public abstract class Persona 
+{
+    //Atributos :
+    protected String nombre;
+    protected int edad;
+    protected String apellido;
+    protected String dni;
+    protected int salario;
+    protected int codigo;
+
+    //Contructores
+
+    public Persona(String nombre, int edad, String apellido, String dni, int salario, int codigo)
+    {
+        
+    }
+
+    
+}
