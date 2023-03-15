624ec16a49a65cf71c786084a0ac5639361f73d6
Pablo <pablojhc96@hotmail.com>
2023-03-15 14:33:57 +0100

    ok

--- code/Unidad9_Interfaces/Clases/Cliente.java
+++ code/Unidad9_Interfaces/Clases/Cliente.java
@@ -1,15 +1,62 @@
 package code.Unidad9_Interfaces.Clases;
 
-public class Cliente 
+public class Cliente implements Comparable
 {
     //Atributo :
-    String nombre;
-    double dinero;
+    private String nombre;
+    private double dinero;
+    private int edad;
 
-    public Cliente(String nombre, double dinero)
+    public void Cliente(String nombre, double dinero)
     {
         this.nombre = nombre;
         this.dinero = dinero;
+    }
+    public void Cliente (String nombre , double dinero, int edad)
+    {
+        this.nombre = nombre;
+        this.dinero = dinero;
+        this. edad = edad;
+    }
+
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+    public double getDinero() {
+        return dinero;
+    }
+
+
+    public void setDinero(double dinero) {
+        this.dinero = dinero;
+    }
+
+    //Método
+    @Override
+    public int compareTo(Object o)
+    {
+        //compaamos por nombre 
+        Cliente c = (Cliente)o;
+        int resultado = 0; //Presuponemos
+        if( this.nombre.compareTo(c.nombre) > 0)
+        {
+            resultado = 1;
+        }
+        else if (this.nombre.compareTo(c.nombre) < 0)
+        {
+            resultado = -1;
+        }
+        return resultado;
+
+
     }
     
 }
