fd1ef9c8f3883abcddebe8c574ef2e5c229b1254
Pablo <pablojhc96@hotmail.com>
2023-03-16 14:04:25 +0100

    ok

--- code/Unidad9_Interfaces/Tarea_Interfaces_Fútbol/JugadoresSpain.java
+++ code/Unidad9_Interfaces/Tarea_Interfaces_Fútbol/JugadoresSpain.java
@@ -15,12 +15,12 @@ public class JugadoresSpain
         // String[] apodo2 = {"de Oro", "de los mares", "de Mijas", "de Titanio", "loca", "Blanco", "Negro"}
 
 
-        Jugador j1 = new Jugador("Juan García", "De oro", 5, 20);
-        Jugador j2 = new Jugador("Pedro Marínez", "Pulga", 9, 15);
-        Jugador j3 = new Jugador("Lucía Ruiz", "de Mijas", 20, 12);
-        Jugador j4 = new Jugador("Carlos Pérez", "De titanio", 5, 9);
-        Jugador j5 = new Jugador("Miguel Hernández", "loca", 19, 12);
-        Jugador j6 = new Jugador("Pedro Sánchez", "El negro mierda", 6, 8);
+        Jugador j1 = new Jugador("Juan García", "De oro");
+        Jugador j2 = new Jugador("Pedro Marínez", "Pulga");
+        Jugador j3 = new Jugador("Lucía Ruiz", "de Mijas");
+        Jugador j4 = new Jugador("Carlos Pérez", "De titanio");
+        Jugador j5 = new Jugador("Miguel Hernández", "loca");
+        Jugador j6 = new Jugador("Pedro Sánchez", "El negro mierda");
     }
     
 }
--- code/Unidad9_Interfaces/Tarea_Interfaces_Fútbol/Jugar_I.java
+++ code/Unidad9_Interfaces/Tarea_Interfaces_Fútbol/Jugar_I.java
@@ -1,3 +1,5 @@
+package code.Unidad9_Interfaces.Tarea_Interfaces_Fútbol;
+
 public interface Jugar_I 
 {
     public void hacerGol();
