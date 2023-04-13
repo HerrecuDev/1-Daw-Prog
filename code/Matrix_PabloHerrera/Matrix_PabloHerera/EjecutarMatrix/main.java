8a45f17a7e3d27eeddc861a4c4c85972072f4a89
Pablo <pablojhc96@hotmail.com>
2023-04-13 12:57:29 +0200

    ok

---
+++ code/Matrix_PabloHerrera/Matrix_PabloHerera/EjecutarMatrix/main.java
@@ -0,0 +1,20 @@
+package code.Matrix_PabloHerrera.Matrix_PabloHerera.EjecutarMatrix;
+
+import java.util.ArrayList;
+
+import code.Matrix_PabloHerrera.Matrix_PabloHerera.Personajes.Personaje;
+import code.Matrix_PabloHerrera.Matrix_PabloHerera.Personajes.Personas_genericas;
+
+public class main 
+{
+    public static void main(String[] args) 
+    {
+        //Generamos 200 PErsonas Genericas :
+        ArrayList<Personas_genericas> listado = new ArrayList<>();
+        
+    }
+
+
+    
+    
+}
