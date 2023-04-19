6e23638efd6bcab23fe63eb7f8bda9ae904af5e7
Pablo <phercue2805@g.educaand.es>
2023-04-19 11:37:43 +0200

    ok

--- code/Unidad9_Interfaces/Tarea_Interfaces_Fútbol/JugadoresSpain.java
+++ code/Unidad9_Interfaces/Tarea_Interfaces_Fútbol/JugadoresSpain.java
@@ -17,33 +17,16 @@ public class JugadoresSpain
         // String[] apodo2 = {"de Oro", "de los mares", "de Mijas", "de Titanio", "loca", "Blanco", "Negro"}
 
 
-        Jugador j1 = new Jugador("Juan García", "De oro" );
-        Jugador j2 = new Jugador("Pedro Marínez", "Pulga");
-        Jugador j3 = new Jugador("Lucía Ruiz", "de Mijas");
-        Jugador j4 = new Jugador("Carlos Pérez", "De titanio");
-        Jugador j5 = new Jugador("Miguel Hernández", "loca");
-        Jugador j6 = new Jugador("Pedro Sánchez", "El negro mierda");
+        // Jugador j1 = new Jugador("Juan García", "De oro" );
+        // Jugador j2 = new Jugador("Pedro Marínez", "Pulga");
+        // Jugador j3 = new Jugador("Lucía Ruiz", "de Mijas");
+        // Jugador j4 = new Jugador("Carlos Pérez", "De titanio");
+        // Jugador j5 = new Jugador("Miguel Hernández", "loca");
+        // Jugador j6 = new Jugador("Pedro Sánchez", "El negro mierda");
 
 
 
-        j1.cometerFalta();
-        j1.cometerFalta();
-        j3.hacerGol();
-        j6.cometerFalta();
-        j1.hacerGol();
-        j1.hacerGol();
-        j6.hacerGol();
-        j6.hacerGol();
-        j6.hacerGol();
-
-        System.out.println(j1.compareTo(j6));
-        OrdenarJugadoresGoles compararGoles = new OrdenarJugadoresGoles();
-        compararGoles.compare(j1, j6);
-
-        System.out.println(j1);
-        System.out.println(j2);
-        System.out.println(j6);
-        //Arrays.sort(Jugad);
+        
     }
     
 }
