6b5c2634f126ddd830f1fec3176b228e6e83f9a2
Pablo <pablojhc96@hotmail.com>
2023-04-20 12:20:29 +0200

    ok

---
+++ code/Unidad10_Ficheros/Proyecto_Libreria_Scanner/src/Clases/Utiles/Lectura_Teclado.java
@@ -0,0 +1,26 @@
+package Clases.Utiles;
+
+import java.util.Scanner;
+
+public class Lectura_Teclado 
+{
+    //Atributo :
+    private Scanner sc;
+
+
+
+    //Contructor : 
+    public Lectura_Teclado(Scanner sc)
+    {
+        this.sc = new Scanner(System.in);
+    }
+
+
+    //Metodos :
+
+    public void ()
+    {
+
+    }
+    
+}
