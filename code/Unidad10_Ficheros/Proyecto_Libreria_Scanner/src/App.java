9bf4558934fd011f76d415778370dd552c710107
Pablo <pablojhc96@hotmail.com>
2023-04-20 12:48:03 +0200

    ok

--- code/Unidad10_Ficheros/Proyecto_Libreria_Scanner/src/App.java
+++ code/Unidad10_Ficheros/Proyecto_Libreria_Scanner/src/App.java
@@ -1,5 +1,10 @@
+import Clases.Utiles.Lectura_Teclado;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws Exception 
+    {
+        System.out.println("------------------------------------");
+        Lectura_Teclado sc = new Lectura_Teclado();
+        int numeroLeido = sc.nextLine;
     }
 }
--- code/Unidad10_Ficheros/Proyecto_Libreria_Scanner/src/Clases/Utiles/Lectura_Teclado.java
+++ code/Unidad10_Ficheros/Proyecto_Libreria_Scanner/src/Clases/Utiles/Lectura_Teclado.java
@@ -18,9 +18,63 @@ public class Lectura_Teclado
 
     //Metodos :
 
-    public void ()
+    public void leerCadena(String cadena)
+    {
+        try 
+        {
+            
+        } 
+        catch (Exception e) 
+        {
+            // TODO: handle exception
+        }
+
+    }
+
+    public int leerInteger(Integer numero)
+    {
+        Integer resultado = null;
+
+        boolean correcto = false;
+        do 
+        {
+            try 
+            {
+                String cadenaLeida = sc.nextLine();
+                resultado = Integer.valueOf(cadenaLeida);
+               
+            } 
+            catch (NumberFormatException excepcion1) 
+            {
+                System.out.println("Dato no valido introduzca un dato valido Por favor");
+            }
+            catch(IllegalAccessException ex2)
+            {
+                System.out.println("Dato no valido , introduzca un dato valido");
+            }
+            catch(Exception ex)
+            {
+                System.out.println("Dato no valido , introduzca un dato valido");
+            }
+            
+        } 
+        while (!correcto);
+        return resultado;
+    }
+    
+
+    public void leerFloat(float numero)
+    {
+        
+    }
+
+
+    public void leerDouble(double numero)
     {
 
     }
-    
+
+
+
+    public void finalizarLectura()
 }
