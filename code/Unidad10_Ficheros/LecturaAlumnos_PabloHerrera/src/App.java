f38274639649a03a80143ad6df606d1593d70c4f
Pablo <pablojhc96@hotmail.com>
2023-04-24 14:37:09 +0200

    ok

---
+++ code/Unidad10_Ficheros/LecturaAlumnos_PabloHerrera/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception 
+    {
+        //PAra ELiminar los espacion en las lineas 
+
+        String[] lineaTroceada = lineaLeida.split();
+        String[] lineaTroceada = lineaLeida.trim();
+
+
+        
+    }
+}
