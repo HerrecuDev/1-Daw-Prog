ef276a715c825a63b8fec77b2ea0134d9ad3a41e
Pablo <pablojhc96@hotmail.com>
2023-04-24 09:55:42 +0200

    ok

--- code/Unidad10_Ficheros/LecturaFicheros/src/App.java
+++ code/Unidad10_Ficheros/LecturaFicheros/src/App.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.nio.Buffer;
+import java.util.Scanner;
 
 public class App 
 {
@@ -81,11 +82,117 @@ public class App
                 fr.close();
             }
         }
+
+
+        System.out.println("--------------------------------------");
+        System.out.println("leo con Scanner");
+
+        int sumaTotal = 0;
+
+        String numeros = "10 34 a6 20";
+        Scanner sc = new Scanner(numeros);
+
+        while(sc.hasNextInt())
+        {
+            int nuevonumero = sc.nextInt();
+            sumaTotal += nuevonumero;
+
+        }
+        System.out.println("Suma Total = " + sumaTotal);
+
+        sc.close();
+        sumaTotal = 0;
+        //Utilizando String :
+
+        String [] trozosNumeros = numeros.split(" ");
+
+        for (int i = 0; i < trozosNumeros.length; i++) 
+        {
+
+            try
+            {
+                int numero = Integer.valueOf(trozosNumeros[i]);
+                sumaTotal += numero;
+            }
+            catch(NumberFormatException NumberFormatException)
+            {
+                System.out.println(NumberFormatException.getMessage());
+            }
+            
+        }
+
+        System.out.println("Suma Total 2 =  " + sumaTotal);
+
+
+
+
+
+        System.out.println("----------------------------");
+
+        System.out.println("Ejercicio PRopuesto en la Pizarrra : Suma de Lineas en el ficheroNumeros.txt");
+        System.out.println();
+
+
+        System.out.println("Leo con FileReader cada linea del fichero");
+
+        
+            fr = new FileReader("src/recursos/ficheroNumeros.txt");
+            bf = new BufferedReader(fr);
+
+            int totalLinea = 0;
+            int linea = 1;
+            String lineaLeida = null;
+        //HAGO UN BUCLE PARA LEER CADA LINEA Y PROCESAR SU SUMA
+         do 
+         {
+            try
+            {
+                totalLinea = 0;
+                lineaLeida = bf.readLine();
+            
+            
+                if (!lineaLeida.isEmpty()) 
+                {
+                    
+                
+                    
+                    String [] lineaTroceada = lineaLeida.split(" ");
+
+                    for (String trozo : lineaTroceada) 
+                    {
+                        try
+                        {
+                            int numero = Integer.valueOf(trozo);
+                            totalLinea += numero;
+                        }
+                        catch(Exception ex)
+                        {
+                            System.out.println("Error : " + ex.getMessage());
+                        }
+                    }
+                    
+                    System.out.println("Suma Linea " + linea + " = " + totalLinea);
+                    
+                }
+                else
+                {
+                    System.out.println( "Linea " + linea + "Vacia");
+                }
+                linea++;
+            }
+            catch(Exception ex)
+            {
+                System.out.println(ex.getMessage());
+            }
+            
+         } while (lineaLeida != null);
+            
+            
+       
+
+       
+
     }
 
 
-
-
-
-    
 }
