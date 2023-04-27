14ebe1f94d33b8dc509317596b1be6854a7e13ab
HerrecuDev <pablojhc15@gmail.com>
2023-04-28 00:06:11 +0200

    ok

---
+++ code/Unidad10_Ficheros/municipios_PabloHerrera/src/utiles/LeerInformacion.java
@@ -0,0 +1,27 @@
+package utiles;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+import clases.Municipio;
+
+public class LeerInformacion
+{
+    public static  ArrayList<Municipio> leerFicheroMunicipio(Integer año) 
+    {
+        String nombreFichero = "PoblacionMunicipiosMalaga.csv";
+        String path = "src/recursos";
+
+        FileReader fileReader = null;
+        BufferedReader bufferedReader = null;
+
+        
+    }
+
+
+
+
+    
+    
+}
