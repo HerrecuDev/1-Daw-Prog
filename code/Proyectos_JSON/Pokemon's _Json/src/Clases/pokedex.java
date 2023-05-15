22a64d9ec726a07d44df5e3624cc8724aec7e1cf
HerrecuDev <pablojhc15@gmail.com>
2023-05-16 00:39:45 +0200

    ok

---
+++ code/Proyectos_JSON/Pokemon's _Json/src/Clases/pokedex.java
@@ -0,0 +1,22 @@
+package Clases;
+
+public class pokedex 
+{
+    //Atributo :
+    private int id;
+    private int numero;
+    private String nombre;
+    private String tipo;
+    private String ataques;
+
+
+    //Contructor:
+
+
+
+
+
+    //Metodos:
+    
+    
+}
---
+++ code/Proyectos_JSON/Pokemon's _Json/src/Clases/pokemon.java
@@ -0,0 +1,16 @@
+package Clases;
+
+public class pokemon 
+{
+    //Atributo :
+    private int numero;
+    private String nombre;
+
+
+    //Contructor:
+
+
+
+    //Metodos:
+    
+}
