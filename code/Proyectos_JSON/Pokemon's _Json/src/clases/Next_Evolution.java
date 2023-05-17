00e9e09e341c9f9bedb59eb5109846e6969e1742
Pablo <pablojhc96@hotmail.com>
2023-05-17 14:40:34 +0200

    ok

---
+++ code/Proyectos_JSON/Pokemon's _Json/src/clases/Next_Evolution.java
@@ -0,0 +1,40 @@
+package clases;
+
+public class Next_Evolution 
+{
+    //Atributo :
+    private int num;
+    private String name;
+
+
+    //Contructores :
+    public Next_Evolution()
+    {
+
+    }
+
+
+    //Métodos:
+
+    public int getNum() {
+        return num;
+    }
+
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
+    
+}
---
+++ code/Proyectos_JSON/Pokemon's _Json/src/utiles/LecturaJson.java
@@ -0,0 +1,18 @@
+package utiles;
+
+import clases.Pokedex;
+
+public class LecturaJson 
+{
+    public static Pokedex leerPokemonJson()
+    {
+        try 
+        {
+            
+
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+    }
+}
