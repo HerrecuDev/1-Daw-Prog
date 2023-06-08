261770eda1d5310f4dc6ebfe551fa2999651bb59
Pablo <phercue2805@g.educaand.es>
2023-06-08 11:40:56 +0200

    ok

--- code/Ejercicios_Relaciones_Recuperacion_Junio/src/Unidad5_Arrays/Ejercicio5.java
+++ code/Ejercicios_Relaciones_Recuperacion_Junio/src/Unidad5_Arrays/Ejercicio5.java
@@ -5,12 +5,37 @@ public class Ejercicio5
     public static void main(String[] args) 
     {
         int[] listaNumeros = new int[10];
+        int maximo = listaNumeros[0];
+        int minimo = listaNumeros[0];
 
         for (int i = 0; i < listaNumeros.length; i++) 
         {
-            listaNumeros[i] = ((int)(Math.random()*51) -30);
-            System.out.print("[" + listaNumeros[i] + "]");
+            listaNumeros[i] = (int)((Math.random()*40) -20);
+           
+           
+           if (listaNumeros[i] > maximo) 
+           {
+                maximo = listaNumeros[i];
+               
+           }
+          
+           if (listaNumeros[i] < minimo) 
+           {
+                minimo = listaNumeros[i];
+              
+           }
+
+          
+            System.out.println("[" +  listaNumeros[i] + "]");
+           
+         
+            
         }
+        System.out.println();
+
+        System.out.println("El numero mayor es = " + maximo);
+        System.out.println("El numero menor es = " + minimo);
+        
         
     }
     
